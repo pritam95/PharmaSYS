1788a4866434ccec4295564e92599ec0e1c30cba
Pritam Sreemani <sreemani.pritam1995@gmail.com>
2021-07-14 00:37:24 +0530

    Application Context added

---
+++ src/com/pharmasys/beans/User.java
@@ -0,0 +1,18 @@
+package com.pharmasys.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/com/pharmasys/constants/Config.java
@@ -0,0 +1,6 @@
+package com.pharmasys.constants;
+
+public class Config {
+
+	public static String APPLICATIONCONTEXT_PATH="/resource/ApplicationContext.xml";
+}
