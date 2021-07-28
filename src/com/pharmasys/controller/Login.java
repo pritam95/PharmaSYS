480026e2df42f7efe48fe84b1576481da9006316
Pritam Sreemani <sreemani.pritam1995@gmail.com>
2021-07-29 00:36:42 +0530

    Hibernate integration with spring

--- src/com/pharmasys/controller/Login.java
+++ src/com/pharmasys/controller/Login.java
@@ -1,5 +1,7 @@
 package com.pharmasys.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pharmasys.beans.User;
 import com.pharmasys.beans.abstrct.IUser;
+import com.pharmasys.service.abstrct.IUserService;
 import com.pharmasys.util.AppBeanFactory;
 
 @Controller
--- src/com/pharmasys/dao/UserDAO.java
+++ src/com/pharmasys/dao/UserDAO.java
@@ -1,28 +1,47 @@
 package com.pharmasys.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
+import javax.transaction.Transactional;
+
+import org.hibernate.FlushMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
 
-import com.pharmasys.beans.User;
 import com.pharmasys.beans.abstrct.IUser;
-import com.pharmasys.constants.Config;
+import com.pharmasys.dao.abstrct.IUserDAO;
 
 @Component
-public class UserDAO {
+@Transactional
+public class UserDAO implements IUserDAO{
+	
+	
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+
 	
 	public void createUser(IUser u) {
-		Configuration con=new Configuration().configure(Config.HIBERNATECONFIG_PATH).addAnnotatedClass(User.class);
+		
+		/*Configuration con=new Configuration().configure(Config.HIBERNATECONFIG_PATH).addAnnotatedClass(User.class);
 		StandardServiceRegistry sr= new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
 	    SessionFactory sf = con.buildSessionFactory(sr);
 	    Session session = sf.openSession();
 	    Transaction tx=session.beginTransaction();
 	    session.save(u);
-	    tx.commit();
+	    tx.commit();*/
+		
+		hibernateTemplate.getSessionFactory().getCurrentSession().setHibernateFlushMode(FlushMode.AUTO);
+		
+		hibernateTemplate.save(u);
+		
 	}
 
 }
