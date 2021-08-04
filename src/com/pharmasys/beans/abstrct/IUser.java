d5a6110b1c3f215c853d8e3d3282551b1e1f6815
Pritam Sreemani <sreemani.pritam1995@gmail.com>
2021-08-05 00:30:35 +0530

    Registration from Register page

--- src/com/pharmasys/beans/abstrct/IUser.java
+++ src/com/pharmasys/beans/abstrct/IUser.java
@@ -4,6 +4,30 @@ import java.util.Date;
 
 public interface IUser {
 	
+	public static String REQUEST_PARAM_f_name = "f_name";
+	
+	public static String REQUEST_PARAM_l_name = "l_name";
+
+	public static String REQUEST_PARAM_gender = "gender";
+	
+	public static String REQUEST_PARAM_dob = "dob";
+	
+	public static String REQUEST_PARAM_address = "address";
+	
+	public static String REQUEST_PARAM_state = "state";
+	
+	public static String REQUEST_PARAM_pinNo = "pinNo";
+	
+	public static String REQUEST_PARAM_phoneNo = "phoneNo";
+	
+	public static String REQUEST_PARAM_emerPhoneNo = "emerPhoneNo";
+
+	public static String REQUEST_PARAM_email = "email";
+	
+	public static String REQUEST_PARAM_userAcces = "userAcces";
+
+	public static String REQUEST_PARAM_password = "password";
+	
 	public long getId();
 	
 	public void setId(long id);
